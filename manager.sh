e3c77b5c57b7d1790d349f8037f3c9fe8ef80365
matheuz-siqueira <matheusbatista.dev@gmail.com>
2022-11-27 17:50:54 -0300

    add menu function

--- manager.sh
+++ manager.sh
@@ -9,7 +9,7 @@
 # Descrição: O programa gerencia a insereção e remoção de   #
 #            usuários e grupos no sistema                   #
 #...........................................................#
-# Exemplo de uso: <Building>                                #
+# Exemplo de uso: ./manager.sh                              #
 #...........................................................#
 # Testado em: 5.1.16                                        #
 #...........................................................#
@@ -20,12 +20,17 @@
 #          24/11/2022, Matheus                              #
 #            - Função Valida Usuário                        #
 #            - Função Deletar Usuaŕio                       #
+#          27/11/2022, Matheus                              #
+#            - Função Deletar Usuário (correção)            #
+#            - Adicionando menu                             #
+#            - Adicionando flag de versão                   #
 #############################################################
 
 #....................VARIÁVEIS....................#
 USER="$(whoami)"
 TEMP=temp.$$
-
+SEP=:
+VERSION="v1.0"
 #....................TESTES....................#
 [ "$USER" != "root" ] && echo "ERRO. apenas o usuário root pode executar o programa." && exit 1
 [ ! "$(which dialog)" ] && echo "Instalando dependência..." && sudo apt-get install dialog > /dev/null 2>&1
@@ -37,9 +42,9 @@ ListUsers()
 {
   while read -r line
   do
-    [ $(echo $line | cut -d : -f 3) -lt 1000 ] && continue
-    [ $(echo $line | cut -d : -f 3) -gt 9999 ] && continue
-    nome="$(echo $line | cut -d : -f 1)"
+    [ $(echo $line | cut -d $SEP -f 3) -lt 1000 ] && continue
+    [ $(echo $line | cut -d $SEP -f 3) -gt 9999 ] && continue
+    nome="$(echo $line | cut -d $SEP -f 1)"
     echo "$nome" >> "$TEMP"
   done < "/etc/passwd"
 
@@ -51,31 +56,57 @@ ValidateUsers()
 {
   while read -r line
   do
-    [ $(echo $line | cut -d : -f 3) -lt 1000 ] && continue
-    [ $(echo $line | cut -d : -f 3) -gt 9999 ] && continue
-    users="$(echo $line | cut -d : -f 1)"
+    [ $(echo $line | cut -d $SEP -f 3) -lt 1000 ] && continue
+    [ $(echo $line | cut -d $SEP -f 3) -gt 9999 ] && continue
+    users="$(echo $line | cut -d $SEP -f 1)"
     echo "$users" >> "$TEMP"
   done < "/etc/passwd"
 
   grep -i -q "$1" "$TEMP"
-  rm -f "$TEMP"
+  return $[ $? ]
+
 }
 
 DeleteUser()
 {
   local user=$(dialog --title "Apagar usuário" --stdout --inputbox "Digite o nome" 6 40 )
-  ValidateUsers "$user"  && {
+  ValidateUsers "$user"
+  if [ $? -eq 0 ]
+  then
     deluser "$user" > /dev/null 2>&1
-    dialog --title "Sucesso!" --msgbox "Usuário deletado com sucesso!" 0 0
-  }
-}
-
-CreateUser()
-{
-  local user=$(dialog --title "Criar usuário" --stdout --inputbox "Digite o nome" 6 40 )
-  ValidateUsers "$user" && {
-    dialog --title "Erro!" --msgbox "Usuário já existe" 0 0
-  }
+    dialog --title "Sucesso!" --msgbox "usuário removido com sucesso!" 6 40
+  else
+    dialog --title "Erro!" --msgbox "Usuário inexistente!" 6 40
+  fi
+  rm -f "$TEMP"
 }
 
 #....................EXECUÇÃO....................#
+
+if [ ! -z $1 ]
+then
+  while getopts "v" OPT
+  do
+    case "$OPT" in
+      "v") echo "$VERSION";;
+    esac
+  done
+  exit 0
+fi
+
+while :
+do
+  action=$(dialog --title "Gerenciamento de Usuários" \
+                  --stdout \
+                  --menu "Escolha uma das opções abaixo:" \
+                  0 0 0 \
+                  Listar "Lista todos os usuários do sistema" \
+                  Inserir "Adiciona um novo usuário no sistema" \
+                  Remover "Remove um usuário do sistema")
+ [ $? -eq 1 ] && exit 0
+ case $action in
+   Listar) ListUsers ;;
+   Remover) DeleteUser ;;
+#  Inserir)  ;;
+ esac
+done
